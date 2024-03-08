e7ac017483eab7a0218d95214b50f1b48ea418fb
Slightlyxz <youresenpai@gmail.com>
2024-03-08 06:47:27 +0100

    Make jumpToFirst work inside Forum Posts again

--- src/userplugins/jumpToFirst/index.tsx
+++ src/userplugins/jumpToFirst/index.tsx
@@ -4,15 +4,11 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
-import { classNameFactory } from "@api/Styles";
-import { getCurrentChannel } from "@utils/discord";
 import definePlugin from "@utils/types";
 import { findByPropsLazy } from "@webpack";
 import { Menu } from "@webpack/common";
+import type { Channel } from "discord-types/general"; // Import the Channel type
 import type React from "react";
-
-
-const cl = classNameFactory("vc-jtf-");
 
 
 const Kangaroo = findByPropsLazy("jumpToMessage");
@@ -22,8 +18,7 @@ export default definePlugin({
     description: "Adds a context menu option to jump to the first message of a channel",
     authors: [{ id: 119536078452424704n, name: "Slightly" }],
     contextMenus: {
-        "message"(children, { id }: { id: string; }) {
-            const channel = getCurrentChannel();
+        "message"(children, { channel }: { channel: Channel; }) {
 
             children.push(
                 <Menu.MenuItem
