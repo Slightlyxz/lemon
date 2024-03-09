889d27c34280009f01e18f2a9a15f2c3a8232532
Slightlyxz <youresenpai@gmail.com>
2024-03-09 23:08:56 +0100

    Update bypassDND

--- src/userplugins/bypassDND/index.tsx
+++ src/userplugins/bypassDND/index.tsx
@@ -5,7 +5,7 @@
  */
 
 import { type NavContextMenuPatchCallback } from "@api/ContextMenu";
-import { DataStore, Notifications } from "@api/index";
+import { Notifications } from "@api/index";
 import { definePluginSettings } from "@api/Settings";
 import { Devs } from "@utils/constants";
 import definePlugin, { OptionType } from "@utils/types";
@@ -13,7 +13,6 @@ import { ChannelStore, Menu, MessageStore, NavigationRouter, PresenceStore, Priv
 import { type Message } from "discord-types/general";
 
 interface IMessageCreate {
-    optimistic: boolean;
     channelId: string;
     guildId: string;
     message: Message;
@@ -29,80 +28,9 @@ function icon(enabled?: boolean) {
     </svg>;
 }
 
-function ContextCallback(name: "guild" | "user" | "channel"): NavContextMenuPatchCallback {
-    return (children, props) => {
-        const type = props[name];
-        if (!type) return;
-        const enabled = bypasses[`${name}s`].includes(type.id);
-        if (name === "user" && type.id === UserStore.getCurrentUser().id) return;
-        children.splice(-1, 0, (
-            <Menu.MenuGroup>
-                <Menu.MenuItem
-                    id={`dnd-${name}-bypass`}
-                    label={`${enabled ? "Remove" : "Add"} DND Bypass`}
-                    icon={() => icon(enabled)}
-                    action={() => {
-                        if (enabled) bypasses[`${name}s`] = bypasses[`${name}s`].filter(id => id !== type.id);
-                        else bypasses[`${name}s`].push(type.id);
-
-                        DataStore.set("bypassdnd", bypasses)
-                            .then(() => {
-                                settings.store[`${name}s`] = bypasses[`${name}s`].join(", ");
-                            })
-                            .catch(error => {
-                                console.error(error);
-                            });
-                    }}
-                />
-            </Menu.MenuGroup>
-        ));
-    };
+function processIds(value) {
+    return value.replace(/\s/g, "").split(",").filter(id => id.trim() !== "").join(", ");
 }
-
-interface Bypasses {
-    guilds: string[];
-    channels: string[];
-    users: string[];
-}
-
-let bypasses: Bypasses;
-
-const settings = definePluginSettings({
-    guilds: {
-        type: OptionType.STRING,
-        description: "Guilds to let bypass (notified when pinged anywhere in guild)",
-        default: "",
-        placeholder: "Separate with commas",
-        onChange: async function (value) {
-            bypasses.guilds = value.replace(/\s/g, "").split(",").filter(id => id.trim() !== "");
-            await DataStore.set("bypassdnd", bypasses);
-        }
-    },
-    channels: {
-        type: OptionType.STRING,
-        description: "Channels to let bypass (notified when pinged in that channel)",
-        default: "",
-        placeholder: "Separate with commas",
-        onChange: async function (value) {
-            bypasses.channels = value.replace(/\s/g, "").split(",").filter(id => id.trim() !== "");
-            await DataStore.set("bypassdnd", bypasses);
-        }
-    },
-    users: {
-        type: OptionType.STRING,
-        description: "Users to let bypass (notified for all messages sent in DMs)",
-        default: "",
-        placeholder: "Separate with commas",
-        onChange: async function (value) {
-            bypasses.users = value.replace(/\s/g, "").split(",").filter(id => id.trim() !== "");
-            await DataStore.set("bypassdnd", bypasses);
-        }
-    },
-    allowOutsideOfDms: {
-        type: OptionType.BOOLEAN,
-        description: "Allow selected users to bypass DND outside of DMs too (acts like a channel/guild bypass, but it's for all messages sent by the selected users)"
-    }
-});
 
 async function showNotification(message: Message, guildId?: string): Promise<void> {
     await Notifications.showNotification({
@@ -115,22 +43,74 @@ async function showNotification(message: Message, guildId?: string): Promise<voi
     });
 }
 
+function ContextCallback(name: "guild" | "user" | "channel"): NavContextMenuPatchCallback {
+    return (children, props) => {
+        const type = props[name];
+        if (!type) return;
+        const enabled = settings.store[`${name}s`].split(", ").includes(type.id);
+        if (name === "user" && type.id === UserStore.getCurrentUser().id) return;
+        children.splice(-1, 0, (
+            <Menu.MenuGroup>
+                <Menu.MenuItem
+                    id={`dnd-${name}-bypass`}
+                    label={`${enabled ? "Remove" : "Add"} DND Bypass`}
+                    icon={() => icon(enabled)}
+                    action={() => {
+                        let bypasses: string[] = settings.store[`${name}s`].split(", ");
+                        if (enabled) bypasses = bypasses.filter(id => id !== type.id);
+                        else bypasses.push(type.id);
+                        settings.store[`${name}s`] = bypasses.filter(id => id.trim() !== "").join(", ");
+                    }}
+                />
+            </Menu.MenuGroup>
+        ));
+    };
+}
+
+const settings = definePluginSettings({
+    guilds: {
+        type: OptionType.STRING,
+        description: "Guilds to let bypass (notified when pinged anywhere in guild)",
+        default: "",
+        placeholder: "Separate with commas",
+        onChange: value => settings.store.guilds = processIds(value)
+    },
+    channels: {
+        type: OptionType.STRING,
+        description: "Channels to let bypass (notified when pinged in that channel)",
+        default: "",
+        placeholder: "Separate with commas",
+        onChange: value => settings.store.channels = processIds(value)
+    },
+    users: {
+        type: OptionType.STRING,
+        description: "Users to let bypass (notified for all messages sent in DMs)",
+        default: "",
+        placeholder: "Separate with commas",
+        onChange: value => settings.store.users = processIds(value)
+    },
+    allowOutsideOfDms: {
+        type: OptionType.BOOLEAN,
+        description: "Allow selected users to bypass DND outside of DMs too (acts like a channel/guild bypass, but it's for all messages sent by the selected users)"
+    }
+});
+
 export default definePlugin({
     name: "BypassDND",
     description: "Still get notifications from specific sources when in do not disturb mode. Right-click on users/channels/guilds to set them to bypass do not disturb mode.",
     authors: [Devs.Inbestigator],
     flux: {
-        async MESSAGE_CREATE({ optimistic, message, guildId, channelId }: IMessageCreate) {
+        async MESSAGE_CREATE({ message, guildId, channelId }: IMessageCreate) {
             try {
                 const currentUser = UserStore.getCurrentUser();
                 const userStatus = await PresenceStore.getStatus(currentUser.id);
-                if (optimistic || message.state === "SENDING" || message.content === "" || message.author.id === currentUser.id || userStatus !== "dnd") {
+                if (message.state === "SENDING" || message.content === "" || message.author.id === currentUser.id || userStatus !== "dnd") {
                     return;
                 }
                 const mentioned = MessageStore.getMessage(channelId, message.id)?.mentioned;
-                if ((bypasses.guilds.includes(guildId) || bypasses.channels.includes(channelId)) && mentioned) {
+                if ((settings.store.guilds.split(", ").includes(guildId) || settings.store.channels.split(", ").includes(channelId)) && mentioned) {
                     await showNotification(message, guildId);
-                } else if (bypasses.users.includes(message.author.id)) {
+                } else if (settings.store.users.split(", ").includes(message.author.id)) {
                     const userChannelId = await PrivateChannelsStore.getOrEnsurePrivateChannel(message.author.id);
                     if (channelId === userChannelId || (mentioned && settings.store.allowOutsideOfDms === true)) {
                         await showNotification(message, guildId);
@@ -146,9 +126,5 @@ export default definePlugin({
         "guild-context": ContextCallback("guild"),
         "channel-context": ContextCallback("channel"),
         "user-context": ContextCallback("user"),
-    },
-    async start() {
-        bypasses = (await DataStore.get("bypassdnd")) ?? { guilds: [], channels: [], users: [] };
-        await DataStore.set("bypassdnd", bypasses);
     }
 });
