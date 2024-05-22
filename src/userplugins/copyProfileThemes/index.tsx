2db7f7916a22e1e7c473912c5c259b735c2963a2
Slightlyxz <youresenpai@gmail.com>
2024-05-22 03:34:29 +0200

    update copyProfileThemes

--- src/userplugins/copyProfileThemes/index.tsx
+++ src/userplugins/copyProfileThemes/index.tsx
@@ -17,6 +17,7 @@
 */
 
 import { sendBotMessage } from "@api/Commands";
+import { NavContextMenuPatchCallback } from "@api/ContextMenu";
 import { ImageIcon } from "@components/Icons";
 import { Devs } from "@utils/constants";
 import definePlugin from "@utils/types";
@@ -43,28 +44,32 @@ function getProfileColors(id: string, guildId?: string) {
     return themeString.trim();
 }
 
+
+const UserContext: NavContextMenuPatchCallback = (children, { user, guildId, channel }: { user: User, guildId: string; channel: Channel; }) => {
+    const profileColors = getProfileColors(user.id, guildId);
+    if (profileColors)
+        children.splice(-1, 0, (
+            <Menu.MenuGroup>
+                <Menu.MenuItem
+                    id="copy-profile-theme"
+                    label="Copy Profile Theme"
+                    action={() => {
+                        sendBotMessage(channel.id, {
+                            content: profileColors
+                        });
+                    }}
+                    icon={ImageIcon}
+                />
+            </Menu.MenuGroup>
+        ));
+};
+
 export default definePlugin({
     name: "CopyProfileThemes",
-    authors: [Devs.KannaDev, Devs.KannaDev],
+    authors: [Devs.KannaDev, Devs.kaitlyn],
     description: "Adds a 'Copy Profile Theme' option to the user context menu to copy the hex codes from a user's profile theme",
+
     contextMenus: {
-        "user-context"(children, { user, guildId, channel }: { user: User, guildId: string; channel: Channel; }) {
-            const profileColors = getProfileColors(user.id, guildId);
-            if (profileColors)
-                children.splice(-1, 0, (
-                    <Menu.MenuGroup>
-                        <Menu.MenuItem
-                            id="copy-profile-theme"
-                            label="Copy Profile Theme"
-                            action={() => {
-                                sendBotMessage(channel.id, {
-                                    content: profileColors
-                                });
-                            }}
-                            icon={ImageIcon}
-                        />
-                    </Menu.MenuGroup>
-                ));
-        }
-    }
+        "user-profile-actions": UserContext
+    },
 });
